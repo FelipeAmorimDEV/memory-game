3ffde141fe07a4640a06aa75b3273152285dc190
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-02 10:56:07 -0300

    ✔  Marcação Inicial

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,22 @@
 const App = () => {
- return <h1>Vite + React Boilerplate</h1>
+  return (
+    <div className="app">
+      <div className="game-status"><span>Status: Jogando</span> <span>Tentativas: 0</span></div>
+      <main className="board">
+        {Array.from({ length: 12 }).map((card, index) => (
+          <div className="flip-card" key={index}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front"></div>
+              <div className="flip-card-back">
+                <h1>Felipe</h1>
+              </div>
+            </div>
+          </div>
+        ))}
+
+      </main>
+    </div>
+  )
 }
 
 export { App }
